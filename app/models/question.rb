3485198ae486c925203eae8a2a36ad4cb2d06c96
Tanguy Billon <tanguy_billon@hotmail.fr>
2019-12-02 15:01:15 +0100

    table

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,7 @@
 class Question < ApplicationRecord
+
+    belongs_to :survey
+    has_many :join_question_responses
+    has_many :responses, through: :join_question_responses
+
 end
--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,2 +1,6 @@
 class Survey < ApplicationRecord
+
+    belongs_to :user
+    has_many :questions
+    
 end
