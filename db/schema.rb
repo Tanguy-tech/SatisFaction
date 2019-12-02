ad4961ed80dfe121425c9d7778540d83175f76fe
Tanguy Billon <tanguy_billon@hotmail.fr>
2019-12-02 15:40:17 +0100

    shema off

--- db/schema.rb
+++ db/schema.rb
@@ -10,61 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_02_142013) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "join_question_responses", force: :cascade do |t|
-    t.bigint "question_id"
-    t.bigint "response_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["question_id"], name: "index_join_question_responses_on_question_id"
-    t.index ["response_id"], name: "index_join_question_responses_on_response_id"
-  end
-
-  create_table "join_response_respondents", force: :cascade do |t|
-    t.bigint "response_id"
-    t.bigint "respondent_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["respondent_id"], name: "index_join_response_respondents_on_respondent_id"
-    t.index ["response_id"], name: "index_join_response_respondents_on_response_id"
-  end
-
-  create_table "questions", force: :cascade do |t|
-    t.integer "number"
-    t.text "content"
-    t.bigint "survey_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["survey_id"], name: "index_questions_on_survey_id"
-  end
-
-  create_table "respondents", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "responses", force: :cascade do |t|
-    t.integer "value"
-    t.bigint "question_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["question_id"], name: "index_responses_on_question_id"
-  end
-
-  create_table "surveys", force: :cascade do |t|
-    t.string "title"
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_surveys_on_user_id"
-  end
-
-  add_foreign_key "join_question_responses", "questions"
-  add_foreign_key "join_question_responses", "responses"
-  add_foreign_key "join_response_respondents", "respondents"
-  add_foreign_key "join_response_respondents", "responses"
 end
