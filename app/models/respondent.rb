2f6a8d9d43e233655713cf7cf6fbbbd3fed008c0
Tanguy Billon <tanguy_billon@hotmail.fr>
2019-12-02 15:17:05 +0100

    table

--- app/models/respondent.rb
+++ app/models/respondent.rb
@@ -1,6 +1,6 @@
 class Respondent < ApplicationRecord
 
     has_many :join_response_respondents
-    has_many :respondents, through: :join_response_respondents
+    has_many :responses, through: :join_response_respondents
 
 end
--- app/models/response.rb
+++ app/models/response.rb
@@ -3,4 +3,7 @@ class Response < ApplicationRecord
     has_many :join_question_responses
     has_many :questions, through: :join_question_responses
 
+    has_many :join_response_respondents
+    has_many :respondents, through: :join_response_respondents
+
 end
