0b0a2a898c507c4f5479a31abb22d87ddfe7da5b
Tanguy Billon <tanguy_billon@hotmail.fr>
2019-12-02 15:35:52 +0100

    table

--- db/migrate/20191202133942_create_questions.rb
+++ db/migrate/20191202133942_create_questions.rb
@@ -1,6 +1,9 @@
 class CreateQuestions < ActiveRecord::Migration[5.2]
   def change
     create_table :questions do |t|
+      t.integer :number
+      t.text :content
+      t.belongs_to :survey, index: true
 
       t.timestamps
     end
