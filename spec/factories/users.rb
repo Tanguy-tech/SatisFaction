f7428ddf119f287134c20f7f7572ffe4dfbfcd6e
Tanguy Billon <tanguy_billon@hotmail.fr>
2019-12-03 11:53:22 +0100

    rspec

---
+++ spec/factories/users.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :random_user, class: User do
+    username { Faker::Internet.username }
+    email { Faker::Internet.email }
+    company { Faker::Company.name }
+    password { Faker::Cannabis.cannabinoid }
+  end
+end
