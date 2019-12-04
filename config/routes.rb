173b267ce5f06cdfe181d3c002073ebe7357396c
baptiste_gfy <geffroy.baptiste@outlook.fr>
2019-12-04 04:03:27 +0100

    ajout des 3 vue about_us, our_services et contact_us

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'static/index_about_us'
+  get 'static/index_our_services'
+  get 'static/index_contact_us'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
