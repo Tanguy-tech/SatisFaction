db84b7414525d57c327246dcd623826c935bb1e1
baptiste_gfy <geffroy.baptiste@outlook.fr>
2019-12-04 18:17:12 +0100

    route pour home page

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
   get 'static/index_our_services'
   get 'static/index_contact_us'
   get 'static/index_home'
- 
+  root 'static#index_home'
+
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
