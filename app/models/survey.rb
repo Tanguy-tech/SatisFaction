10357ded797dfdc4e7b1179f2e5c722660241ada
Tanguy Billon <tanguy_billon@hotmail.fr>
2019-12-04 15:12:33 +0100

    forms

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -6,7 +6,8 @@ class Survey < ApplicationRecord
     validates :user_id,
             presence: true
             
-    belongs_to :user
-    has_many :questions
+    belongs_to :user,
+    has_many :questions, dependent: :destroy
+    accepts_nested_attributes_for :questions, allow_destroy: true
     
 end
