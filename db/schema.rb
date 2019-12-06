a859f8706245109332888da621d7b1ad9a341056
baptiste_gfy <geffroy.baptiste@outlook.fr>
2019-12-06 15:14:39 +0100

    commit migration list ??? why ???

--- db/schema.rb
+++ db/schema.rb
@@ -40,9 +40,9 @@ ActiveRecord::Schema.define(version: 2019_12_06_115129) do
   end
 
   create_table "questions", force: :cascade do |t|
-    t.bigint "survey_id"
     t.integer "number"
     t.text "content"
+    t.bigint "survey_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["survey_id"], name: "index_questions_on_survey_id"
@@ -88,5 +88,4 @@ ActiveRecord::Schema.define(version: 2019_12_06_115129) do
   add_foreign_key "join_question_responses", "responses"
   add_foreign_key "join_response_respondents", "respondents"
   add_foreign_key "join_response_respondents", "responses"
-  add_foreign_key "questions", "surveys"
 end
