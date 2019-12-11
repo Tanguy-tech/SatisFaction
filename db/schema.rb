e08534037fc37af246607e5ea3d852537cdaf54e
RibJulien <ju.ribeaucourt@gmail.Com>
2019-12-11 16:08:39 +0100

    add front reset password

--- db/schema.rb
+++ db/schema.rb
@@ -30,6 +30,24 @@ ActiveRecord::Schema.define(version: 2019_12_10_160319) do
     t.text "message"
   end
 
+  create_table "join_question_responses", force: :cascade do |t|
+    t.bigint "question_id"
+    t.bigint "response_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["question_id"], name: "index_join_question_responses_on_question_id"
+    t.index ["response_id"], name: "index_join_question_responses_on_response_id"
+  end
+
+  create_table "join_response_respondents", force: :cascade do |t|
+    t.bigint "response_id"
+    t.bigint "respondent_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["respondent_id"], name: "index_join_response_respondents_on_respondent_id"
+    t.index ["response_id"], name: "index_join_response_respondents_on_response_id"
+  end
+
   create_table "lists", force: :cascade do |t|
     t.text "email"
     t.datetime "created_at", null: false
@@ -37,12 +55,25 @@ ActiveRecord::Schema.define(version: 2019_12_10_160319) do
   end
 
   create_table "questions", force: :cascade do |t|
-    t.bigint "survey_id"
+    t.integer "number"
     t.text "content"
-    t.boolean "answered"
+    t.bigint "survey_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["survey_id"], name: "index_questions_on_survey_id"
+  end
+
+  create_table "respondents", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "responses", force: :cascade do |t|
+    t.integer "value"
+    t.bigint "question_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["question_id"], name: "index_responses_on_question_id"
   end
 
   create_table "surveys", force: :cascade do |t|
@@ -70,5 +101,8 @@ ActiveRecord::Schema.define(version: 2019_12_10_160319) do
 
   add_foreign_key "answers", "questions"
   add_foreign_key "answers", "surveys"
-  add_foreign_key "questions", "surveys"
+  add_foreign_key "join_question_responses", "questions"
+  add_foreign_key "join_question_responses", "responses"
+  add_foreign_key "join_response_respondents", "respondents"
+  add_foreign_key "join_response_respondents", "responses"
 end
