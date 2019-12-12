fcace878cca76143287a910f28e26fc167329460
Tanguy Billon <tanguy_billon@hotmail.fr>
2019-12-12 17:11:57 +0100

    Back landing

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -13,11 +13,15 @@ class AnswersController < ApplicationController
 
 
   def create
+    puts '='*90
+    puts params 
+    puts '='*90
     @answer = Answer.create(answer_params)
     @question = Question.find(@answer.question_id)
     @survey = Survey.find(@answer.survey_id)
+    @landing = Landing.where(survey_id: @survey)
     # @question.update(answered: true) => allow to respond only once 
-    redirect_to survey_path(@survey)
+    redirect_to landing_path(@landing.ids)
   end
 
 
