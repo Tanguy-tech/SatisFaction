da1b27fddbd24694333a3ac9b8b146d1bdfa4ab6
Sacha TORDJMAN <sacha.tordjman@gmail.com>
2019-12-13 13:51:07 +0100

    Update list mailer and view show suvey

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,15 +6,15 @@ class ListsController < ApplicationController
   	@list = List.create!(list_params)
 
   	respond_to do |format|
-	format.html { redirect_to surveys_path }
-	flash[:create_email] = 'Email was successfully send.'
-	end
+			format.html { redirect_to surveys_path }
+			flash[:create_email] = 'Email was successfully send.'
+		end
 
-	@list_array = @list.email.split(', ')
+		@list_array = @list.email.split(', ')
 
-	@list_array.each do |list|
-    	ListMailer.sending_survey(list).deliver_now
-    end
+		@list_array.each do |list|
+	    ListMailer.sending_survey(list).deliver_now
+	  end
 
   end
 
@@ -23,7 +23,7 @@ class ListsController < ApplicationController
 private
 
 	def list_params
-		params.require(:list).permit(:email)
+		params.require(:list).permit(:email, :survey_id, :landing_id)
 	end
 
 end
